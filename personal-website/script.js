49ba04f6881daf206ea0613964704659229bd4da
Shannon Fajemiyo <149418997+shann0nf@users.noreply.github.com>
2025-12-31 00:00:58 +0000

    Delete personal-website directory

--- personal-website/script.js
+++
@@ -1,7 +0,0 @@
-// Set footer year
-document.getElementById("year").textContent = new Date().getFullYear();
-
-// Smooth scroll to projects on button click
-document.getElementById("btnProjects").addEventListener("click", () => {
-  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
-});
